c98658158fe46ed00b47bb346be732814d87b502
Gren <124205591+Grenstafox@users.noreply.github.com>
2025-03-23 22:29:07 -0600

    abre.php

--- php/abre.php
+++ php/abre.php
@@ -1,7 +1,7 @@
 <?php
 
     //$conexion = new mysqli("fdb1030.awardspace.net", "4585643_formu", "QD?m?+K{9(2C!uby", "4585643_formu");
-    $conexion = new mysqli("localhost", "root", "", "empresa");    
+    $conexion = new mysqli("https://github.com/Grenstafox/Empresa", "alumno17.marco.jesus@outlookc.com", "17JMal@#", "empresa");    
     if($conexion){
         echo "Conexion establecida";
     }else{
